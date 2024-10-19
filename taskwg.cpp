e132b60e77b2181dce1fc5509b2d71c3d7b7b3bd
Ditya Hidayat <aditataby@gmail.com>
2024-10-19 19:07:24 +0700

    First Commit

---
+++ taskwg.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    // Menampilkan pesan ke layar
+    std::cout << "Selamat datang di program C++!" << std::endl;
+    std::cout << "Program ini hanya menampilkan beberapa kalimat sederhana." << std::endl;
+    std::cout << "Semoga harimu menyenangkan!" << std::endl;
+
+    return 0;
+}
